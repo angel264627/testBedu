d3875c2c0e64c085bcb4dce6c78481a73e5da08b
angel264627 <105018838+angel264627@users.noreply.github.com>
2022-05-06 00:59:36 -0500

    Add files via upload

---
+++ Vectores.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Programas;
+
+/**
+ *
+ * @author angel
+ */
+public class Vectores{
+	public static void main(String args[]){
+		
+		int numeros[] = new int[5];
+		
+		numeros[0] = 5;
+		numeros[1] = 220;
+		numeros[2] = 8;
+		numeros[3] = 458;
+		numeros[4] = 22;
+		
+		System.out.print("[" + numeros [0] + "]");
+		System.out.print("[" + numeros [1] + "]");
+		System.out.print("[" + numeros [2] + "]");
+		System.out.print("[" + numeros [3] + "]");
+		System.out.print("[" + numeros [4] + "]");
+				
+	}
+}
